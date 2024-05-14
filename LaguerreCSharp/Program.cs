f73872bb5fa904b7663d0564ecb12cc65c920e0b
Viblazer <golafoned@gmail.com>
2024-05-14 23:45:49 +0300

    add Gauss function

--- LaguerreCSharp/Program.cs
+++ LaguerreCSharp/Program.cs
@@ -1,15 +1,88 @@
 ﻿using LaguerreClasses;
 
-// double l = 1;
-// double mu = l * 3;
-// double GaussianNormalDistribution(double t) => 1 / (l * Math.Sqrt(2 * Math.PI)) * Math.Exp(-Math.Pow(t - mu, 2) / 2 * Math.Pow(l, 2));
-
-// LaguerreTabulator Instance = new(GaussianNormalDistribution, 10000, 2, 4, 15, 0.001);
-// (int[] a, double[] b) = Instance.TabulateLaguerreTransformation();
-// for (int n = 0; n < a.Length; n++)
-// {
-//     Console.WriteLine($"For lambda={l} u={mu} beta=2 and sigma=4 Transformation value: n={a[n]} transformed=P{b[n]}");
-// }
+using (StreamWriter writetext = new("data/GaussReverseTransformationData.csv"))
+{
+    int a = 10;
+    int N = 20;
+    int beta = 2;
+    int sigma = 4;
+    List<(string, Laguerre)> laguerres =
+    [
+        (
+            "Gauss(μ=3λ λ=1)",
+            new(
+                (double t) =>
+                    1
+                    / (1 * Math.Sqrt(2 * Math.PI))
+                    * Math.Exp(-Math.Pow(t - 3, 2) / 2 * Math.Pow(1, 2)),
+                100,
+                beta,
+                sigma,
+                N,
+                0.001
+            )
+        ),
+        (
+            "Gauss(μ=6λ λ=1)",
+            new(
+                (double t) =>
+                    1
+                    / (1 * Math.Sqrt(2 * Math.PI))
+                    * Math.Exp(-Math.Pow(t - 6, 2) / 2 * Math.Pow(1, 2)),
+                100,
+                beta,
+                sigma,
+                N,
+                0.001
+            )
+        ),
+        (
+            "Gauss(μ=3λ λ=2)",
+            new(
+                (double t) =>
+                    1
+                    / (2 * Math.Sqrt(2 * Math.PI))
+                    * Math.Exp(-Math.Pow(t - 6, 2) / 2 * Math.Pow(2, 2)),
+                100,
+                beta,
+                sigma,
+                N,
+                0.001
+            )
+        ),
+        (
+            "Gauss(μ=6λ λ=2)",
+            new(
+                (double t) =>
+                    1
+                    / (2 * Math.Sqrt(2 * Math.PI))
+                    * Math.Exp(-Math.Pow(t - 6, 2) / 2 * Math.Pow(2, 2)),
+                100,
+                beta,
+                sigma,
+                N,
+                0.001
+            )
+        ),
+    ];
+    writetext.Write("x");
+    foreach (var item in laguerres)
+    {
+        writetext.Write($",{item.Item1},{item.Item1}_transformed");
+    }
+    writetext.WriteLine();
+    foreach (double x in Service.GenerateLinspace(1, a, 100 * (a - 1)))
+    {
+        writetext.Write($"{x}");
+        foreach (var laguerre in laguerres)
+        {
+            writetext.Write(
+                $",{laguerre.Item2.Func(x)},{laguerre.Item2.ReverseLaguerreTransformation(x)}"
+            );
+        }
+        writetext.WriteLine();
+    }
+}
 
 static double func(double x) => 1 / (x + 1);
 LaguerreTabulator Instance = new(func, 100, 2, 4, 10, 0.001);
@@ -50,58 +123,55 @@ using (StreamWriter writetext = new("data/lauguerreFuncData.csv"))
     }
 }
 
-static double funcSonya(double t)
-{
-    if (t != 0)
-        return Math.Sin(Math.PI / 3) - Math.Atan(t + t / 2) / t;
-    else
-        return 0;
-}
-
-static double funcDemian(double t)
-{
-    return 2 * (Math.PI / 2 - Math.Atan(t + Math.PI / 6));
-}
-
-static double funcIvan(double t)
-{
-    if (t >= 0 && t <= 2 * Math.PI)
-        return Math.PI / 3 - Math.Sin(t + 3 * Math.PI / 2);
-    else
-        return 0;
-}
-
-static double funcStefa(double t)
-{
-    if (t >= 0 && t <= 2 * Math.PI)
-        return -1d / 200d * Math.Sin(t) * Math.Pow(Math.E, t);
-    else
-        return 0;
-}
-
-static double funcYuliia(double t)
-{
-    return Math.Cos(t) / Math.Pow(t, t);
-}
-List<(string, Laguerre)> laguerres =
-[
-    new("funcSonya", new Laguerre(funcSonya, 100, 2, 4, 10)),
-    new("funcDemian", new Laguerre(funcDemian, 100, 2, 4, 10)),
-    new("funcIvan", new Laguerre(funcIvan, 100, 2, 4, 10)),
-    new("funcStefa", new Laguerre(funcStefa, 100, 2, 4, 10)),
-    new("funcYuliia", new Laguerre(funcYuliia, 100, 2, 4, 10)),
-];
-
 using (StreamWriter writetext = new("data/reverseTransformationData.csv"))
 {
-    int N = 20;
+    static double funcSonya(double t)
+    {
+        if (t != 0)
+            return Math.Sin(Math.PI / 3) - Math.Atan(t + t / 2) / t;
+        else
+            return 0;
+    }
+
+    static double funcDemian(double t)
+    {
+        return 2 * (Math.PI / 2 - Math.Atan(t + Math.PI / 6));
+    }
+
+    static double funcIvan(double t)
+    {
+        if (t >= 0 && t <= 2 * Math.PI)
+            return Math.PI / 3 - Math.Sin(t + 3 * Math.PI / 2);
+        else
+            return 0;
+    }
+
+    static double funcStefa(double t)
+    {
+        if (t >= 0 && t <= 2 * Math.PI)
+            return -1d / 200d * Math.Sin(t) * Math.Pow(Math.E, t);
+        else
+            return 0;
+    }
+
+    static double funcYuliia(double t)
+    {
+        return Math.Cos(t) / Math.Pow(t, t);
+    }
+    List<(string, Laguerre)> laguerres =
+    [
+        new("funcSonya", new Laguerre(funcSonya, 100, 2, 4, 10)),
+        new("funcDemian", new Laguerre(funcDemian, 100, 2, 4, 10)),
+        new("funcIvan", new Laguerre(funcIvan, 100, 2, 4, 10)),
+        new("funcStefa", new Laguerre(funcStefa, 100, 2, 4, 10)),
+        new("funcYuliia", new Laguerre(funcYuliia, 100, 2, 4, 10)),
+    ];
     int a = 6;
     writetext.Write("x");
     foreach (var item in laguerres)
     {
         writetext.Write($",{item.Item1},{item.Item1}_transformed");
     }
-    for (int i = 0; i < N; i++) { }
     writetext.WriteLine();
 
     foreach (double x in Service.GenerateLinspace(1, a, 100 * (a - 1)))
